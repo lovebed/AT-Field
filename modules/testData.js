736843b480b46dd627c45592c3ee4cb4b2674749
zodiacg <zodiacg@hotmail.com>
2013-05-03 14:00:02 +0800

    meaningless testData

---
+++ modules/testData.js
@@ -0,0 +1,31 @@
+var testWebReqList = [];
+var testCookieList = [];
+var testWhiteList = [];
+
+webReqRule = function(pattern,domain,flag){
+    this.pattern = pattern;
+    this.domain = domain;
+    this.flag = flag;
+}
+
+cookieRule = function(dompat,keypat){
+    this.domain = dompat;
+    this.keypat = keypat;
+}
+
+//construct webreqlist
+for(var i=0;i<10;i++){
+    testWebReqList[i] = new webReqRule("pattern"+i,"domain"+i,true);
+}
+
+for(var i=0;i<10;i++){
+    testCookieList[i] = new cookieRule("domain"+i,"keypat"+i);
+}
+
+for(var i=0;i<10;i++){
+    testWhiteList[i] = "domain"+i;
+}
+
+console.log(testWhiteList);
+console.log(testCookieList);
+console.log(testWebReqList);
